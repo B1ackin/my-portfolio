5e468e3fdbe5950f45102d8fc397053eb5c06459
B1ackin <xx13.02@gmail.com>
2020-10-13 14:16:08 +0300

    Add two blocks

---
+++ src/main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from "./Main.module.css"
+
+
+
+function Main() {
+    return (
+        <div className={style.mainBlock}>
+
+        </div>
+    );
+}
+
+export default Main;

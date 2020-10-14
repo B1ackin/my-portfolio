63965e8a3c52e3f11ddb5fc734ff36392a540501
B1ackin <xx13.02@gmail.com>
2020-10-14 20:31:37 +0300

    refactor title

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,7 +6,12 @@ import style from "./Main.module.css"
 function Main() {
     return (
         <div className={style.mainBlock}>
-
+            <div className={style.text}>
+                <span>Hi There</span>
+                <h1>I am Jeffrey Aaron</h1>
+                <p>Front end developer</p>
+            </div>
+            <div className={style.photo}></div>
         </div>
     );
 }

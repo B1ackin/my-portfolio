ab9da97861723a12c8a7cd9ddedd56b660bd5dbc
B1ackin <xx13.02@gmail.com>
2020-10-17 14:23:53 +0300

    complite lesson 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import Header from "./header/Header";
 import Main from "./main/Main";
+import Skills from "./skills/Skills";
+import Skill from "./skills/skill/Skill";
 
 
 
@@ -10,6 +12,8 @@ function App() {
     <div className="App">
         <Header/>
         <Main />
+        <Skills />
+
     </div>
   );
 }
---
+++ src/skills/Skills.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import style from "./Skills.module.css"
+import styleContainer from "../common/styles/Container.module.css"
+import Skill from "./skill/Skill";
+
+
+function Skills() {
+    return (
+        <div className={style.skillsBlock}>
+            <div className= {`${styleContainer.container} ${style.skillsContainer}`}>
+                <h2 className={style.title}>MY SKILLS</h2>
+                <div className={style.skills}>
+                    <Skill title={"JS"} decription={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eos esse eum explicabo"}/>
+                    <Skill title={"CSS"} decription={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eos esse eum explicabo"}/>
+                    <Skill title={"React"} decription={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam eos esse eum explicabo"}/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
